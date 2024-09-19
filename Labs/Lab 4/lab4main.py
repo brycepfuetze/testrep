1a9713d7e6e79afce4bc8f382bee11bf21b7b7cc
brycepfuetze <brpf9496@colorado.edu>
2024-09-19 11:05:00 -0600

    work for lab4

---
+++ Labs/Lab 4/lab4main.py
@@ -0,0 +1,82 @@
+
+# import libraries
+import numpy as np
+
+
+def driver():
+    # test functions
+    f1 = lambda x: 1+0.5*np.sin(x)
+    # fixed point is alpha1 = 1.4987....
+    f2 = lambda x: 3+2*np.sin(x)
+    #fixed point is alpha2 = 3.09...
+    Nmax = 100
+    tol = 1e-6
+    # test f1 '''
+    x0 = 0.0
+    [x, xstar,ier] = fixedpt(f1,x0,tol,Nmax)
+    print('the approximate fixed point is:',xstar)
+    print('f1(xstar):',f1(xstar))
+    print('Error message reads:',ier)
+    print('All are ', x)
+    #test f2 '''
+    x0 = 0.0
+    # [x, xstar,ier] = fixedpt(f2,x0,tol,Nmax)
+    # print('the approximate fixed point is:',xstar)
+    # print('f2(xstar):',f2(xstar))
+    # print('Error message reads:',ier)
+
+    [fit, diff1, diff2] = compute_order(x, xstar)
+
+    
+def driver2():
+    # test functions
+    f1 = lambda x: (10/(x+4))**(1/2)
+    # fixed point is alpha1 = 1.3652300134140976
+    Nmax = 100
+    tol = 1e-6
+    # test f1 '''
+    x0 = 1.5
+    [x, xstar,ier] = fixedpt(f1,x0,tol,Nmax)
+    print('the approximate fixed point is:',xstar)
+    print('f1(xstar):',f1(xstar))
+    print('Error message reads:',ier)
+    print('All are ', x)
+
+    [fit, diff1, diff2] = compute_order(x, xstar)
+
+
+# define routines
+def fixedpt(f,x0,tol,Nmax):
+    ''' x0 = initial guess'''
+    ''' Nmax = max number of iterations'''
+    ''' tol = stopping tolerance'''
+    count = 0
+    x = np.array(x0)
+
+    while (count <Nmax):
+        count = count +1
+        x1 = f(x0)
+        x = np.append(x, x0)
+        if (abs(x1-x0) <tol):
+            xstar = x1
+            ier = 0
+            return [np.delete(x, -1), xstar,ier]
+        x0 = x1
+    xstar = x1
+    ier = 1
+    return [np.delete(x, -1), xstar, ier]
+
+def compute_order(x, xstar):
+    diff1 = np.abs(x[1::]-xstar)
+    diff2 = np.abs(x[0:-1]-xstar)
+    fit = np.polyfit(np.log(diff2.flatten()), np.log(diff1.flatten()),1)
+    print('the order of the equation is')
+    print('log(|p_{n+1} -p|) = log(lambda) + alpha*log(|p_n-p|) where:')
+    print('lambda = ' + str(np.exp(fit[1])))
+    print('alpha = ' + str(fit[0]))
+    return [fit, diff1, diff2]
+
+driver2()
+
+
+
