daf6d525b5dd2a9fca4c5bc2d388a8cda3126eac
brycepfuetze <brpf9496@colorado.edu>
2024-11-14 11:44:59 -0700

    Lab 11

--- Homework/HW 10/problem1.py
+++ Homework/HW 10/problem1.py
@@ -23,10 +23,10 @@ err_a = abs(T6-ra)
 err_b = abs(T6-rb)
 err_c = abs(T6-rc)
 
-trace0 = go.Scatter(x= x, y=ra, name='Numerator - cubic, Denominator - cubic', yaxis='y1')
-trace1 = go.Scatter(x= x, y=rb, name='Numerator - quadratic, Denominator - quartic', yaxis='y1')
-trace2 = go.Scatter(x= x, y=rc, name='Numerator - quartic, Denominator - quadratic', yaxis='y1')
-trace3 = go.Scatter(x= x, y=y, name='actual', yaxis='y1')
+trace0 = go.Scatter(x= x, y=ra, name='(a) Numerator - cubic, Denominator - cubic', yaxis='y1')
+trace1 = go.Scatter(x= x, y=rb, name='(b) Numerator - quadratic, Denominator - quartic', yaxis='y1')
+trace2 = go.Scatter(x= x, y=rc, name='(c) Numerator - quartic, Denominator - quadratic', yaxis='y1')
+trace3 = go.Scatter(x= x, y=y, name='Exact', yaxis='y1')
 trace4 = go.Scatter(x= x, y=T6, name='6th order Maclaurin', yaxis='y1')
 
 data = [trace0, trace1, trace2, trace3, trace4]
@@ -35,15 +35,15 @@ layout = go.Layout(title='Pade Polynomial Comparison for sin(x)',
                   xaxis=dict(
                       title='x'),
                   yaxis=dict(
-                          title='error',
+                          title='y',
                           anchor='x'),
                   )
 fig = go.Figure(data=data, layout=layout)
 fig.show()
 
-trace0 = go.Scatter(x= x, y=err_a, name='Numerator - cubic, Denominator - cubic', yaxis='y1')
-trace1 = go.Scatter(x= x, y=err_b, name='Numerator - quadratic, Denominator - quartic', yaxis='y1')
-trace2 = go.Scatter(x= x, y=err_c, name='Numerator - quartic, Denominator - quadratic', yaxis='y1')
+trace0 = go.Scatter(x= x, y=err_a, name='(a) Numerator - cubic, Denominator - cubic', yaxis='y1')
+trace1 = go.Scatter(x= x, y=err_b, name='(b) Numerator - quadratic, Denominator - quartic', yaxis='y1')
+trace2 = go.Scatter(x= x, y=err_c, name='(c) Numerator - quartic, Denominator - quadratic', yaxis='y1')
 
 data = [trace0, trace1, trace2]
 layout = go.Layout(title='Pade Polynomial Comparison for sin(x)',
